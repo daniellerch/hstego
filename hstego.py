23ae3a98bef67ad02b6e4870ebec93f9cba01ae8
Daniel Lerch <dlerch@gmail.com>
2022-07-13 11:24:51 +0200

    doc

--- hstego.py
+++ hstego.py
@@ -12,13 +12,13 @@ SPATIAL_EXT = ["png", "pgm", "tif"]
 
 def help():
     print("\nUsage:")
-    print("  hstego.py embed <msg file> <cover image> <output stego image> [password]")
-    print("  hstego.py extract <stego image> <output msg file> [password]")
-    print("  hstego.py capacity <image>")
+    print(f"  {sys.argv[0]} embed <msg file> <cover image> <output stego image> [password]")
+    print(f"  {sys.argv[0]} extract <stego image> <output msg file> [password]")
+    print(f"  {sys.argv[0]} capacity <image>")
 
     print("\nExample:")
-    print("  hstego.py embed input-secret.txt cover.png stego.png p4ssw0rd")
-    print("  hstego.py extract stego.png output-secret.txt p4ssw0rd")
+    print(f"  {sys.argv[0]} embed input-secret.txt cover.png stego.png p4ssw0rd")
+    print(f"  {sys.argv[0]} extract stego.png output-secret.txt p4ssw0rd")
     print("")
     sys.exit(0)
 
